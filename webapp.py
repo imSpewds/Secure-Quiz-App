3d205a7d858a5338f6834d23e0368ae80e88af38
RosTobic <54326493+RosTobic@users.noreply.github.com>
2020-01-27 09:52:22 -0800

    sites up

--- webapp.py
+++ webapp.py
@@ -1,1 +1,17 @@
+import os
+from flask import Flask, url_for, render_template, request
+from flask import redirect
+from flask import session
 
+app = Flask(__name__)
+
+@app.route('/')
+def renderMain():
+    return render_template('home.html')
+
+@app.route('/page1')
+def renderPage1():
+    return render_template('page1.html')
+
+if __name__=="__main__":
+    app.run(debug=True)

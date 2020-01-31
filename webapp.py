40ad162a9582df92164e4ecad5036f659ab64eed
legosrockdude86 <arriagabrandon231@gmail.com>
2020-01-31 09:11:19 -0800

    a
    a

--- webapp.py
+++ webapp.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, url_for, render_template, request, Markup
+from flask import Flask, url_for, render_template, request
 from flask import redirect
 from flask import session
 
@@ -7,25 +7,25 @@ app = Flask(__name__)
 
 app.secret_key=os.environ["SECRET_KEY"];
 
-
-@app.route('/')
+@app.route('/')[]
 def renderMain():
     return render_template('home.html')
 
 @app.route('/startOver')
 def startOver():
     session.clear()
+
     return redirect('/')
 
 @app.route('/page1', methods=['GET', 'POST'])
 def renderPage1():
     return render_template('page1.html')
-    
+
 @app.route('/page2', methods=['GET', 'POST'])
 def renderPage2():
     session['firstQ'] = request.form['firstQ']
     return render_template('page2.html')
-    
+
 @app.route('/page3', methods=['GET', 'POST'])
 def renderPage3():
     session['secondQ'] = request.form['secondQ']
@@ -34,37 +34,7 @@ def renderPage3():
 @app.route('/page4', methods=['GET', 'POST'])
 def renderpage4():
     session['thirdQ'] = request.form['thirdQ']
-    return render_template('page4.html', isRight1 = isRight1(), isRight2 = isRight2(), isRight3 = isRight3())
-    
-def isRight1():
-    first = session['firstQ']
-    first.lower()
-    answer = ""
-    if first == "asia":
-        answer = answer + Markup("Correct")
-    else:
-        answer = answer + Markup("Wrong")
-    return answer
-    
-def isRight2():
-    second = session['secondQ']
-    second.lower()
-    answer = ""
-    if second == "mount everest":
-        answer = answer + Markup("Correct")
-    else:
-        answer = answer + Markup("Wrong")
-    return answer
-    
-def isRight3():
-    third = session['thirdQ']
-    third.lower()
-    answer = ""
-    if third == "florida":
-        answer = answer + Markup("Correct")
-    else:
-        answer = answer + Markup("Wrong")
-    return answer
-    
+    return render_template('page4.html')
+
 if __name__=="__main__":
     app.run(debug=True)
